e2049e7d52c721f634edce3c8493d3cbebf13d5f
LouisAugustine <nzlouis.com@gmail.com>
2024-09-18 20:24:07 +1200

    Fix App test issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import Books from "./components/Books";
+import logo from "./logo.svg";
+import "./App.css";
 
-const queryClient = new QueryClient();
-
-const App: React.FC = () => {
-  const isStorybook = !!process.env.STORYBOOK;
+function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      {isStorybook ? (
-        <>
-          <Books />
-          {/* You can add other components for Storybook preview here */}
-        </>
-      ) : (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Books />} />
-            <Route path="*" element={<Books />} />
-          </Routes>
-        </BrowserRouter>
-      )}
-    </QueryClientProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/components/Books.type.ts
+++ src/components/Books.type.ts
@@ -39,6 +39,25 @@ export interface OpenLibraryAuthor {
   name: string;
 }
 
+export interface Book {
+  id: string;
+  title: string;
+  subtitle: string;
+  type: string;
+  format: string;
+  releaseDate: string;
+  author: string;
+  price: number;
+  publisherRRP: number;
+  pages: number;
+  description: string;
+  dimensions: string;
+  wishList: boolean;
+  bestSeller: boolean;
+  isbn: string;
+  publisher: string;
+}
+
 export interface Genre {
   id: string;
   name: string;
